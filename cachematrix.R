ad72560b5528da49846aed20f9de4fb322d850b5
flarreta <f.larreta@gmail.com>
2019-09-26 18:23:42 -0500

    FL. Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
-## Put comments here that give an overall description of what your
-## functions do
+## It might be ineficient to re calculate in a function when a value has already being calculated. 
+## The following functions uses inverse matrixes already calculated and saved as cached data trying 
+## to make the process of obtaining the inverse of a matrix more agile.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates an "special" vector and creates a function that gets the mean 
+##of it´s values.
 
 makeCacheMatrix <- function(x = matrix()) {
     m <- NULL
@@ -19,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve check if the mean of the "special" vector has been calculated and cached.
+## If it has been already calculated, it returns the cached mean, if it has not been calculated, 
+## it finds it.
 
 cacheSolve <- function(x, ...) {
     m <- x$getinverse()
@@ -31,10 +35,10 @@ cacheSolve <- function(x, ...) {
     m <- solve(data, ...)
     x$setinverse(m)
     m
-        ## Return a matrix that is the inverse of 'x'
 }
 
 j <- matrix(1:4, 2, 2)
 
 
 k <- makeCacheMatrix(j)
+cacheSolve(k)
